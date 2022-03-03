497145dcba5e5eee0074d463cff14eeb9bf9a67f
Oziel Pacheco <latikakm@gmail.com>
2022-03-03 16:22:19 -0600

    Se añade gestion del pdf

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -3,6 +3,7 @@ import { of } from 'rxjs';
 import { NgWizardConfig, NgWizardService, StepChangedArgs, StepValidationArgs, STEP_STATE, THEME } from 'ng-wizard';
 import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 
+//Modelos con sus  datas
 import { generos } from 'src/app/data/generos';
 import { Genero } from 'src/app/models/genero';
 
@@ -11,6 +12,11 @@ import { Nacionalidad } from 'src/app/models/nacionalidad';
 
 import { clubes } from 'src/app/data/clubes';
 import { Club } from 'src/app/models/club';
+//Pdf
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas';
+
+
 
 
 @Component({
@@ -26,11 +32,14 @@ export class FormComponent implements OnInit {
   public clubs : any;
   //variables para la credencial
   public fullName:string ='';
+  public age:string;
   public gender:Genero;
   public nationality: Nacionalidad;
   public club: Club;
   public ocu: string;
   public rfcx: string;
+
+  
 
   constructor( private ngWizarService: NgWizardService) {
     //Asignamos los datas de arrays a una variable para poder recorrerlos
@@ -40,7 +49,7 @@ export class FormComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    //console.log(this.genders)
+    console.log(this.genders);
   }
 
   //Reactive Form
@@ -53,7 +62,7 @@ export class FormComponent implements OnInit {
     idNationality : new FormControl('',Validators.required),
     idClub : new FormControl('',Validators.required),
     ocupation : new FormControl('',Validators.required),
-    rfc : new FormControl('')
+    rfc : new FormControl('')//No es requerido ya que dependemos de la edad del usuario
   })
 
 
@@ -64,13 +73,60 @@ onSubmit(){
   this.gender = this.genders.find(element => element.id == this.formFMF.get('idGender').value);
   this.nationality = this.nationalities.find(element => element.idN == this.formFMF.get('idNationality').value);
   this.club = this.clubs.find(element => element.id == this.formFMF.get('idClub').value);
+  //lo asignamos a una variable
+  this.age = this.formFMF.get('birthdate').value;
   this.ocu = this.formFMF.get('ocupation').value;
   this.rfcx = this.formFMF.get('rfc').value;
 
 }
 
 
+downloadPDF() {
+  // Extraemos el
+  const DATA = document.getElementById('htmlData');
+  const doc = new jsPDF('p', 'pt', 'a4');
+  const options = {
+    background: 'white',
+    scale: 3
+  };
+  html2canvas(DATA, options).then((canvas) => {
 
+    const img = canvas.toDataURL('image/PNG');
+
+    // Add image Canvas to PDF
+    const bufferX = 15;
+    const bufferY = 15;
+    const imgProps = (doc as any).getImageProperties(img);
+    const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+    return doc;
+  }).then((docResult) => {
+    docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+  });
+}
+
+
+// public downloadPDF(): void {
+
+
+//   const doc = new jsPDF();
+   
+  
+//    doc.text('Estos son tus datos!', 10, 10);
+//    doc.text('Nombre: '+this.fullName, 10, 20);
+//    doc.text('Fecha de nacimiento: '+this.age, 10, 30);
+//    doc.text('Genero: '+this.gender.nombre, 10, 40);
+//    doc.text('Nacionalidad: '+this.nationality.nombre, 10, 50);
+//    doc.text('Club seleccionado: '+this.club.nombre, 10, 60);
+//    doc.text('Ocupacion: '+this.ocu, 10, 70);
+//    doc.text('RFC: '+this.rfcx, 10, 80);
+//    doc.save('MisDatos.pdf');
+// }
+
+
+
+//Esto es del steper
   stepStates = {
     normal: STEP_STATE.normal,
     disabled: STEP_STATE.disabled,
